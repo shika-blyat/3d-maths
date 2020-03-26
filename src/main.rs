3a8602432e1e2c313d1eff024a500f076ce457a1
Shika-Blyat <abdelzighem1@gmail.com>
2020-03-26 20:06:02 +0100

    optimize line drawing algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,45 @@
 mod png_gen;
 use png_gen::gen_png;
 
-fn is_in_line(
+fn draw_pixel(x: usize, y: usize, vec: &mut [u8], width: usize, (r, g, b): (u8, u8, u8)) {
+    vec[(500 - x) * 3 * width + (500 - y) * 3] = r;
+    vec[(500 - x) * 3 * width + (500 - y) * 3 + 1] = g;
+    vec[(500 - x) * 3 * width + (500 - y) * 3 + 2] = b;
+}
+fn draw_line(
     (begin_x, begin_y): (f32, f32),
     (end_x, end_y): (f32, f32),
-) -> impl Fn(f32, f32) -> bool {
+    vec: &mut [u8],
+    width: usize,
+) {
     // y = ax + b or y = gradient * x + offset
     let gradient = (end_y - begin_y) / (end_x - begin_x);
-    let offset = end_y - (gradient * end_x);
     let (biggest_x, smallest_x) = if begin_x > end_x {
         (begin_x, end_x)
     } else {
         (end_x, begin_x)
     };
-    let (biggest_y, smallest_y) = if begin_y > end_y {
+    let (_, mut smallest_y) = if begin_y > end_y {
         (begin_y, end_y)
     } else {
         (end_y, begin_y)
     };
-    move |x, y| {
-        if x > biggest_x || x < smallest_x || y < smallest_y || y > biggest_y {
-            false
-        } else {
-            y == x * gradient + offset
-        }
+    for x in (smallest_x as u32..biggest_x as u32).rev() {
+        smallest_y += gradient;
+        draw_pixel(
+            x as usize,
+            smallest_y.round() as usize,
+            vec,
+            width,
+            (255, 255, 255),
+        );
     }
 }
 
-fn is_in_square(
-    (x1, y1): (f32, f32),
-    (x2, y2): (f32, f32),
-    (x3, y3): (f32, f32),
-    (x4, y4): (f32, f32),
-) -> impl Fn(f32, f32) -> bool {
-    /*
-        1------2
-        |      |
-        |      |
-        4------3
-    */
-    let side_size = x2 - x1;
-    let (center_x, center_y) = (x3 - x1, y1 - y3);
-    move |x, y| {
-        let max = x.max(y);
-        if max == x {
-            (max - center_x).abs() == side_size
-        } else {
-            (max - center_y).abs() == side_size
-        }
-    }
-}
 fn main() {
-    let mut vec = vec![];
-    let a1 = (400.0, 250.0);
-    let a2 = (250.0, 400.0);
-    let a3 = (250.0, 100.0);
-    let a4 = (100.0, 250.0);
-    let is_in = is_in_square(a1, a2, a3, a4);
-    let height = 500;
-    let width = 500;
-    let mut cntr = 0;
-    for y in (0..height).rev() {
-        for x in 0..width {
-            if is_in(x as f32, y as f32) {
-                cntr += 1;
-                vec.push(150);
-                vec.push(200);
-                vec.push(100);
-            } else {
-                vec.push(0);
-                vec.push(0);
-                vec.push(0);
-            }
-        }
-    }
-    println!("{}", cntr);
-    gen_png(vec, width);
+    const HEIGHT: usize = 500;
+    const WIDTH: usize = 500;
+    let mut vec = vec![0; (HEIGHT * WIDTH) * 3];
+    draw_line((400., 250.), (250., 400.), &mut vec, WIDTH);
+    gen_png(&vec, WIDTH as u32, HEIGHT as u32);
 }
--- src/png_gen.rs
+++ src/png_gen.rs
@@ -2,14 +2,14 @@ use std::fs::File;
 use std::io::BufWriter;
 use std::path::Path;
 
-pub fn gen_png(vec: Vec<u8>, width: u32) {
+pub fn gen_png(vec: &[u8], width: u32, height: u32) {
     // For reading and opening files
 
     let path = Path::new(r"image.png");
     let file = File::create(path).unwrap();
     let ref mut w = BufWriter::new(file);
 
-    let mut encoder = png::Encoder::new(w, width, vec.len() as u32 / width / 3);
+    let mut encoder = png::Encoder::new(w, width, height);
     encoder.set_color(png::ColorType::RGB);
     encoder.set_depth(png::BitDepth::Eight);
     let mut writer = encoder.write_header().unwrap();
